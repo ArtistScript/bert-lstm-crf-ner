61be0ccf70fd94da428f0a96532c6eb5629bdf1f
戴朱涛 <artistscript@outlook.com>
2019-04-17 17:17:28 +0800

    打印预测结果

--- train/bert_lstm_ner_cg_estimator.py
+++ train/bert_lstm_ner_cg_estimator.py
@@ -672,13 +672,12 @@ def train(args):
         sess.run(train_op,feed_dict={input_ids:train_data['input_ids'],input_mask:train_data['input_mask'],
                                      segment_ids:train_data['segment_ids'],label_ids:train_data['label_ids']})
         if i%10==0:
-            train_summary, accu = sess.run([merged,accuracy], feed_dict={input_ids:train_data['input_ids'],input_mask:train_data['input_mask'],
+            train_summary, accu, prediction = sess.run([merged,accuracy,pred_ids], feed_dict={input_ids:train_data['input_ids'],input_mask:train_data['input_mask'],
                                      segment_ids:train_data['segment_ids'],label_ids:train_data['label_ids']})
             train_writer.add_summary(train_summary, i)
             print('saving summary at %s, accuracy %s'%(i,accu))
-            # print(sess.run(tf.shape(pred_ids),feed_dict={input_ids:train_data['input_ids'],input_mask:train_data['input_mask'],
-            #                      segment_ids:train_data['segment_ids'],label_ids:train_data['label_ids']}))
-            # print(train_data['label_ids'].shape)
+            print(prediction)
+            print(train_data['label_ids'])
     train_writer.close()
 
 
