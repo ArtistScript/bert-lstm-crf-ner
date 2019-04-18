dcd64934fad7990a4c7e9dc3c3a1b29ec2adfd3f
戴朱涛 <artistscript@outlook.com>
2019-04-18 15:14:46 +0800

    gpu使用率设置

--- train/bert_lstm_ner_cg_estimator.py
+++ train/bert_lstm_ner_cg_estimator.py
@@ -664,6 +664,7 @@ def train(args):
     # init_global= tf.global_variables_initializer()
     config = tf.ConfigProto()
     config.gpu_options.allow_growth = True#动态申请显存
+    config.gpu_options.per_process_gpu_memory_fraction = 0.7
     sess = tf.Session(config=config)
     sess.run(tf.local_variables_initializer())
     sess.run(tf.global_variables_initializer())
